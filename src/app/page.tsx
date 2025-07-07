8198fea1a433597e53f860a160ada14feec6d0ec
Steropse <vaceslavtata858@gmail.com>
2025-07-07 09:51:19 +0600

    feat(shadcn): add button and styles for it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button'
 import styles from './page.module.scss'
 const Home = () => {
   return (
     <div className={styles['container']}>
       <h1>Платформа независимых театров</h1>
+      <Button> Test </Button>
     </div>
   )
 }
