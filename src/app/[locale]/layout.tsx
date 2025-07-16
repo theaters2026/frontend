c9deddd91b97a88b71f9a64364a4887a95f308c5
Velial-Re <defflldd@gmail.com>
2025-07-16 20:47:32 +0600

    feat: migrate to App Router with locale support - Create dynamic locale-based layout in [locale]/layout.tsx - Implement generateMetadata for localized metadata - Configure NextIntlClientProvider for client-side translations

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,87 @@
+import { NextIntlClientProvider, hasLocale } from 'next-intl'
+import { notFound } from 'next/navigation'
+import { routing } from '@/shared/i18n/routing'
+import type { Metadata } from 'next'
+import '@/shared/styles/globals.scss'
+import localFont from 'next/font/local'
+
+const spectral = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/spectral/spectral-bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-spectral',
+  display: 'swap',
+})
+
+const spectralSemiBold = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/spectral/spectral-semibold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-spectral-semibold',
+  display: 'swap',
+})
+
+const onest = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/onest/onest-regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-onest',
+  display: 'swap',
+})
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>
+}): Promise<Metadata> {
+  const { locale } = await params
+
+  const title =
+    locale === 'en'
+      ? 'Platform of independent theatres'
+      : 'Платформа независимых театров'
+  const description =
+    locale === 'en'
+      ? 'Platform of independent theatres'
+      : 'Платформа независимых театров'
+
+  return {
+    title,
+    description,
+  }
+}
+
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
+}) {
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  return (
+    <html lang={locale}>
+      <body
+        className={`${spectral.variable} ${spectralSemiBold.variable} ${onest.variable}`}
+      >
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+import { useTranslations } from 'next-intl'
+
+export default function Home() {
+  const t = useTranslations('HomePage')
+
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+    </div>
+  )
+}
