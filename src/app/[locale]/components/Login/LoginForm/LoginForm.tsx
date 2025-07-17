793754af125d95423073ee964896c1b8703343e9
Steropse <vaceslavtata858@gmail.com>
2025-07-17 19:53:15 +0600

    feat: add interaction with API to Login component

--- src/app/[locale]/components/Login/LoginForm/LoginForm.tsx
+++ src/app/[locale]/components/Login/LoginForm/LoginForm.tsx
@@ -7,18 +7,49 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Form } from '@/shared/ui/form'
 import { Input } from '@/shared/ui/input'
 import { useTranslations } from 'next-intl'
+import axios, { type AxiosResponse } from 'axios'
+
+type RespType = {
+  username: string
+  password: string
+  email: string
+}
 
 export const LoginForm: React.FC = () => {
   const t = useTranslations('Login')
   const form = useForm<LoginFormSchema>({
     resolver: zodResolver(loginFormSchema),
     defaultValues: {
-      email: '',
+      username: '',
       password: '',
+      email: '',
     },
   })
 
-  const onSubmit = (data: LoginFormSchema) => {
+  const onSubmit = async (data: LoginFormSchema) => {
+    await axios
+      .post<RespType>(`http://localhost:3000/auth/login`, {
+        username: data.username,
+        password: data.password,
+        email: data.email,
+      })
+      .then((response: AxiosResponse) => {
+        console.log('Login confirm', response)
+      })
+      .catch(async (response: AxiosResponse) => {
+        await axios
+          .post<RespType>(`http://localhost:3000/auth/register`, {
+            username: data.username,
+            password: data.password,
+            email: data.email,
+          })
+          .then((response: AxiosResponse) => {
+            console.log('Login confirm', response)
+          })
+          .catch(async (response: AxiosResponse) => {
+            console.log('Login confirm', response)
+          })
+      })
     console.log('Form data:', data)
   }
 
@@ -71,6 +102,18 @@ export const LoginForm: React.FC = () => {
           <div>
             <Button type="submit"> {t('buttonSubmit')} </Button>
           </div>
+          {/* <div>
+            <Button type="button" onClick={async () => {
+              await axios
+                .get<RespType>(`http://localhost:3000/api#/auth/register`)
+                .then((response: AxiosResponse) => {
+                  console.log('Login confirm')
+                })
+                .catch(async (response: AxiosResponse) => {
+                  console.log('Login confirm')
+                })
+            }}> O </Button>
+          </div> */}
         </form>
       </Form>
     </div>
