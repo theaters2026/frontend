52b39ba77bb611ea6b224804b391290d37e0ace5
Velial-Re <defflldd@gmail.com>
2025-07-17 17:31:32 +0600

    feat(components): add EventCard component
    - Create EventCard component with TypeScript
    - Add responsive SCSS styles
    - Implement event display logic

---
+++ src/app/[locale]/components/EventCard/EventCard.tsx
@@ -0,0 +1,87 @@
+'use client'
+import Image from 'next/image'
+import { Button } from '@/shared/ui'
+import styles from './EventCard.module.scss'
+import { UI_CONSTANTS } from '@/shared/constants'
+import { useNavigation, useEventCard } from '@/shared/hooks'
+import { useTranslations } from 'next-intl'
+import { EventCardData } from '@/shared/types'
+
+interface EventCardProps {
+  eventData: EventCardData
+}
+
+export const EventCard = ({ eventData }: EventCardProps) => {
+  const { navigateToEvent } = useNavigation()
+  const t = useTranslations('EventCard')
+
+  const { localizedLocation, localizedDate, localizedTheater } = useEventCard({
+    date: eventData.date,
+    location: eventData.location,
+    theater: eventData.theater,
+  })
+
+  const handleDetailsClick = () => {
+    navigateToEvent(eventData.id)
+  }
+
+  return (
+    <article className={styles['card']}>
+      <header className={styles['card__header']}>
+        <figure>
+          <Image
+            src={eventData.imageSrc}
+            alt={eventData.imageAlt}
+            width={UI_CONSTANTS.EVENT_CARD_IMAGE.WIDTH}
+            height={UI_CONSTANTS.EVENT_CARD_IMAGE.HEIGHT}
+            priority
+            className={styles['card__image']}
+          />
+        </figure>
+        <span className={styles['card__price']}>
+          {eventData.price} {UI_CONSTANTS.CURRENCY_SYMBOL}
+        </span>
+        <span className={styles['card__age-rating']}>
+          {eventData.ageRating}
+        </span>
+      </header>
+      <div className={styles['card__content']}>
+        <h3 className={styles['card__title']}>{eventData.title}</h3>
+        <div className={styles['card__meta']}>
+          <div
+            className={`${styles['meta__column']} ${styles['meta__column--left']}`}
+          >
+            <span className={`${styles['card__theater']} text--small`}>
+              {localizedTheater}
+            </span>
+            <time
+              className={`${styles['card__date']} text--small`}
+              dateTime={localizedDate}
+            >
+              {localizedDate}
+            </time>
+          </div>
+          <div
+            className={`${styles['meta__column']} ${styles['meta__column--right']}`}
+          >
+            <span className={`${styles['card__location']} text--small`}>
+              {localizedLocation}
+            </span>
+          </div>
+        </div>
+      </div>
+      <footer className={styles['card__footer']}>
+        <Button
+          onClick={handleDetailsClick}
+          className={styles['card__button']}
+          size="md"
+        >
+          {t('details')}
+        </Button>
+        <Button className={styles['card__button']} size="md">
+          {t('buyTicket')}
+        </Button>
+      </footer>
+    </article>
+  )
+}
