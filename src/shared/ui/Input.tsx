9566bdf288f8b5d19955a56ddf393e713b49f52d
Velial-Re <defflldd@gmail.com>
2025-07-19 11:15:05 +0600

    refactor: rename UI components
    - Rename input.tsx to Input.tsx
    - Rename label.tsx to Label.tsx

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { cn } from '@/shared/utils/index'
+import { cn } from '@/shared/utils'
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, type, ...props }, ref) => {
